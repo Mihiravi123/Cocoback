29492df28ccd77db22280abe51367ad762f5888a
Nipuni91 <nipunimihi91@gmail.com>
2024-10-26 22:17:11 +0530

    webconfig 4

--- src/main/java/com/example/cocoloco/Web/WebConfig.java
+++ src/main/java/com/example/cocoloco/Web/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Allows CORS for all API endpoints
-                .allowedOrigins("https://cocoadminpanel.onrender.com") // Replace with your React app's URL
+                .allowedOrigins("https://cocoadminpanel.onrender.com", "https://cocofrontend.onrender.com") // Allows multiple origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
                 .allowedHeaders("*"); // Allows all headers
     }
